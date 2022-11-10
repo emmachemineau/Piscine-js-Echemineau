49168f45a0fc84200408320f1d90553358634d22
emmachemineau <113505949+emmachemineau@users.noreply.github.com>
2022-11-10 15:35:06 +0100

    Create ex10.js

---
+++ ex10.js
@@ -0,0 +1,16 @@
+const i=prompt ("entrer un nombre");
+
+
+if (i % 15 == 0) {
+console.log(i + " " + "cyberpunk2077");
+}
+else if (i % 5 === 0 ){
+console.log(i + " " + "PUNK");
+}
+else if (i % 3 === 0 ) {
+console.log(i + " " + "Cyber");
+}
+else {
+console.log(i)
+}
+ 
